e4bc8aa3a7d98153e601e34e48208d93fa3d74f3
Pechenux <belokoniy@gmail.com>
2024-12-07 13:50:03 +0300

    feat: add main page (#6)

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,18 +1,24 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useLayoutEffect } from 'react';
 import HomeBackground from '@/assets/backgrounds/home.svg?component';
-import { ModalContainer } from '@/components/common/layout/ModalContainer';
-import { Button } from '@/components/common/uikit';
+import UnidataLogo from '@/assets/images/unidata-logo.svg?component';
+import { Button, ExternalLink } from '@/components/common/uikit';
 import styles from './home.module.scss';
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const handleOpen = useCallback(() => {
-    setIsOpen(true);
-  }, []);
-  const handleClose = useCallback(() => {
-    setIsOpen(false);
+  const router = useRouter();
+
+  // let video overflow without adding scrollbar
+  useLayoutEffect(() => {
+    document.documentElement.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.documentElement.style.overflow = '';
+      document.body.style.overflow = '';
+    };
   }, []);
 
   return (
@@ -23,12 +29,49 @@ export default function Home() {
         height="100%"
         preserveAspectRatio="xMidYMid slice"
       />
-      <button onClick={handleOpen}>Click me!</button>
-      <ModalContainer title="Hello!" isOpen={isOpen} onClose={handleClose}>
-        <div>asd</div>
-        <div>dsa</div>
-        <Button>Click me too!</Button>
-      </ModalContainer>
+      <div className={styles.hero}>
+        <div className={styles.content}>
+          <h1 className={styles.name}>Desbordante</h1>
+          <h6 className={styles.description}>
+            Open-source data profiling tool{' '}
+            <span className={styles.supported}>
+              supported by
+              <a
+                className={styles.unidataLogo}
+                href="https://universe-data.ru"
+                target="_blank"
+                rel="noreferrer"
+                title="Universe Data"
+              >
+                <UnidataLogo width={144} height={48} />
+              </a>
+            </span>
+          </h6>
+          <div className={styles.links}>
+            <Button
+              variant="gradient"
+              onClick={() => router.push('/create-task/choose-primitive')}
+            >
+              Get Started
+            </Button>
+            <div className={styles.external}>
+              <ExternalLink href="https://github.com/desbordante">
+                GitHub
+              </ExternalLink>
+              {/* Link is broken */}
+              {/* <ExternalLink href="https://mstrutov.github.io/Desbordante">
+                User Guide
+              </ExternalLink> */}
+            </div>
+          </div>
+        </div>
+        <div className={styles.videoContainer}>
+          <video autoPlay muted loop>
+            <source src="static/videos/hero-animation.webm" type="video/webm" />
+          </video>
+        </div>
+      </div>
+      <div style={{ height: '2000px' }}>asd</div>
     </div>
   );
 }
--- src/types/withClassname.d.ts
+++ src/types/withClassname.d.ts
@@ -1,3 +1,3 @@
 export type WithClassname = {
-  className: string;
+  className?: string;
 };
