d0438d7d161d8061d2e9d8811f61e22c2fa7b2e7
Liana <samosha_yul@icloud.com>
2025-01-06 01:38:12 +0300

    feat: add pie chart statistics

--- src/app/reports/fd/[tab]/page.tsx
+++ src/app/reports/fd/[tab]/page.tsx
@@ -1,6 +1,7 @@
 import { ReportsLayout } from '@/components/common/layout/ReportsLayout';
 import { Icon } from '@/components/common/uikit';
 import { FDResult, Snippet } from '@/components/reports';
+import { ChartStatistics } from '@/components/reports/ChartStatisctics';
 import styles from './page.module.scss';
 
 export default async function FDReport({
@@ -9,6 +10,12 @@ export default async function FDReport({
   params: Promise<{ tab: string }>;
 }) {
   const tabs = [
+    {
+      name: 'statistics',
+      label: 'Statistics',
+      icon: <Icon name="chart" />,
+      content: <ChartStatistics />,
+    },
     {
       name: 'result',
       label: 'Primitive list',
--- src/components/reports/index.ts
+++ src/components/reports/index.ts
@@ -3,3 +3,5 @@ export * from './Filters';
 export * from './ScrollableTable';
 export * from './FDResult';
 export * from './Snippet';
+export * from './PieChart';
+export * from './ChartStatisctics';
